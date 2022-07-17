a2bf7ad3466735bfbd066c19218ee367bddfb32a
Struan Bartlett <struan.bartlett@NewsNow.co.uk>
2022-07-17 06:12:15 +0000

    Extend installation instructions

--- build-utils/install.sh
+++ build-utils/install.sh
@@ -7,10 +7,19 @@ if mountpoint $MNT >/dev/null; then
   rm -rf $MNT/*
   cp -a /opt/dkvm/* $MNT/
 
+  cat <<_EOE_ >&2
+Now enable DKVM for Docker (and, experimentally, Podman), by running:
+
+/opt/dkvm/scripts/install.sh
+_EOE_
+
 else
-  echo "Usage: docker run --rm -v /opt/dkvm:$MNT dkvm" >&2
-  echo >&2
-  echo " - Installs dkvm to host:/opt/dkvm" >&2
-  echo " - N.B. /opt/dkvm is hardcoded and requires a rebuild to be changed" >&2
-  
+  cat <<"_EOE_" >&2
+Usage: docker run --rm -v /opt/dkvm:$MNT dkvm
+
+ - Installs dkvm package to host:/opt/dkvm
+ - N.B. /opt/dkvm is hardcoded and requires a rebuild to be changed
+
+_EOE_
+
 fi
