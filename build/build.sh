945c7708e69788217ed6111b3e81329b7203be3e
Struan Bartlett <struan.bartlett@NewsNow.co.uk>
2022-07-18 09:22:32 +0100

    Update REPO

--- build/build.sh
+++ build/build.sh
@@ -2,9 +2,8 @@
 
 REPO=newsnowlabs/dkvm
 
-DOCKER_BUILDKIT=1 docker build -t dkvm:latest .
+DOCKER_BUILDKIT=1 docker build -t $REPO .
 
 echo >&2
 echo "To install, now run: docker run --rm -v /opt/dkvm:/dkvm $REPO" >&2
 echo >&2
-
