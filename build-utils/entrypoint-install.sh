69934fe4eda57aa3d6d9d4d80a64d1b9c1b0b777
Struan Bartlett (Admin) <struan.bartlett@NewsNow.co.uk>
2023-03-20 21:29:56 +0000

    Improve entrypoint installation instructions

--- build-utils/entrypoint-install.sh
+++ build-utils/entrypoint-install.sh
@@ -3,21 +3,21 @@
 MNT=/runcvm
 REPO=newsnowlabs/runcvm
 
-if mountpoint $MNT >/dev/null; then
+if mountpoint $MNT >/dev/null 2>&1; then
 
-  #rm -rf $MNT/*
-  #cp -a /opt/runcvm/* $MNT/
   rsync -aR --delete /opt/runcvm/./ $MNT/
 
   echo "RunCVM install/upgrade successful!" >&2
   echo >&2
 else
 
-  cat <<"_EOE_" >&2
+  cat <<_EOE_ >&2
+ERROR: Host bind-mount not specified, see below for correct usage.
+
 Usage: docker run --rm -v /opt/runcvm:$MNT $REPO
 
- - Installs runcvm package to host:/opt/runcvm
- - N.B. /opt/runcvm is hardcoded and requires a rebuild to be changed
+ - Installs runcvm package to the host at /opt/runcvm
+   (installation elsewhere is currently unsupported)
 
 _EOE_
 
